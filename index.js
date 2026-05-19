d9594ebd7610e7bd0072b63275f33bd930a0cb64
kauanr-0l <kauanrodrigues63624@gmail.com>
2026-05-19 20:39:42 -0300

    atualizado 1.0

---
+++ index.js
@@ -0,0 +1,22 @@
+let idPersonagem = 1;
+
+const nome = document.getElementById("nome");
+const especie = document.getElementById("especie");
+const status = document.getElementById("status");
+const genero = document.getElementById("genero");
+const origem = document.getElementById("origem");
+const imagem = document.getElementById("imagem");
+
+const inputPesquisa =
+document.getElementById("pesquisa");
+
+const botaoBuscar =
+document.getElementById("buscar");
+
+const botaoVoltar =
+document.getElementById("voltar");
+
+const botaoAvancar =
+document.getElementById("avancar");
+
+
